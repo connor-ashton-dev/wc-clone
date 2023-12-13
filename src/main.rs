d886d2d8679177b8d511052a29de213b4b8778e1
Spencer Bartholomew <38776747+spencerbart@users.noreply.github.com>
2023-12-13 12:34:50 -0700

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::{env, fs};
 
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let mut flags: Vec<&String> = vec![];
     let mut file_name: Option<&String> = None;
 
-    for arg in &args[1..] {
+    for arg in args.iter().skip(1) {
         if arg.starts_with('-') {
             flags.push(arg)
         } else if arg.contains('.') {
@@ -13,21 +13,15 @@ fn main() {
         }
     }
 
-    let file_name = match file_name {
-        Some(name) => name,
-        None => {
-            eprintln!("No file name provided");
-            return;
-        }
-    };
+    let file_name = file_name.ok_or_else(|| {
+        eprintln!("No file name provided");
+        "No file name provided".to_string()
+    })?;
 
-    let file_string = match fs::read_to_string(file_name) {
-        Ok(content) => content,
-        Err(_) => {
-            eprintln!("Failed to read file: {}", file_name);
-            return;
-        }
-    };
+    let file_string = fs::read_to_string(file_name).map_err(|e| {
+        eprintln!("Failed to read file: {} because {}", file_name, e);
+        "Failed to read file".to_string()
+    })?;
 
     if flags.is_empty() {
         println!(
@@ -37,27 +31,29 @@ fn main() {
             get_bytes(&file_string),
             file_name
         )
-    } else {
-        for flag in &flags {
-            match flag.as_str() {
-                "-c" => {
-                    println!("{} {}", get_bytes(&file_string), file_name)
-                }
-                "-l" => {
-                    println!("{} {}", get_lines(&file_string), file_name)
-                }
-                "-w" => {
-                    println!("{} {}", get_words(&file_string), file_name)
-                }
-                "-m" => {
-                    println!("{} {}", get_chars(&file_string), file_name)
-                }
-                _ => {
-                    println!("Invalid flag")
-                }
+    }
+
+    for flag in flags {
+        match flag.as_str() {
+            "-c" => {
+                println!("{} {}", get_bytes(&file_string), file_name)
+            }
+            "-l" => {
+                println!("{} {}", get_lines(&file_string), file_name)
+            }
+            "-w" => {
+                println!("{} {}", get_words(&file_string), file_name)
+            }
+            "-m" => {
+                println!("{} {}", get_chars(&file_string), file_name)
+            }
+            _ => {
+                println!("Invalid flag")
             }
         }
     }
+
+    Ok(())
 }
 
 fn get_bytes(s: &str) -> usize {
